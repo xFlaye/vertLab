db6c4389ab0c59151093eae267d1cc965af459bf
xFlaye <eyala@yorku.ca>
2022-02-15 23:38:45 -0500

    Add empty  project

---
+++ mqtt/Assets/MQTT/getData.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getData : MonoBehaviour
+{
+    
+    [SerializeField]
+    private MQTT mqttObj;
+    
+   
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Debug.Log("from getData" + mqttObj.myMsg);
+        float mover = mqttObj.myMsg;
+        float normalization = 0.0f;
+
+        normalization = 1/mover + Random.Range(-10.0f, 10.0f); 
+
+        gameObject.transform.position = new Vector3(normalization,0,0);
+        
+    }
+}
---
+++ mqtt/Assets/MQTT/tmpMQTT.cs
@@ -0,0 +1,78 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using System.Text;
+// using uPLibrary.Networking.M2Mqtt;
+// using System.Net.Security;
+// using uPLibrary.Networking.M2Mqtt.Messages;
+// using System.Security.Cryptography.X509Certificates;
+// using System;
+
+// public class MQTT : MonoBehaviour
+// {
+//     //http://tdoc.info/blog/2014/11/10/mqtt_csharp.html
+//     private MqttClient client;
+//     // The connection information
+//     public string brokerHostname = "ec2-11-111-11.us-west-2.compute.amazonaws.com";
+//     public int brokerPort = 8883;
+//     public string userName = "test";
+//     public string password = "test";
+//     public TextAsset certificate;
+//     // listen on all the Topic
+//     static string subTopic = "#";
+
+//     void Start()
+// 	{
+// 		if (brokerHostname != null && userName != null && password != null)
+// 		{
+// 			Debug.Log("connecting to " + brokerHostname + ":" + brokerPort);
+// 			Connect();
+// 			client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+// 			byte[] qosLevels = { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE };
+// 			client.Subscribe(new string[] { subTopic }, qosLevels);
+// 		}
+// 	}
+
+//     private void Connect()
+// 	{
+// 		Debug.Log("about to connect on '" + brokerHostname + "'");
+// 		// Forming a certificate based on a TextAsset
+// 		X509Certificate cert = new X509Certificate();
+// 		//cert.Import(certificate.bytes);
+// 		Debug.Log("Using the certificate '" + cert + "'");
+// 		client = new MqttClient(brokerHostname, brokerPort, true, null/*cert*/, null, MqttSslProtocols.TLSv1_0, MyRemoteCertificateValidationCallback);
+// 		string clientId = Guid.NewGuid().ToString();
+// 		Debug.Log("About to connect using '" + userName + "' / '" + password + "'");
+// 		try
+// 		{
+// 			client.Connect(clientId, userName, password);
+// 		}
+// 		catch (Exception e)
+// 		{
+// 			Debug.LogError("Connection error: " + e);
+// 		}
+// 	}
+
+//     public static bool MyRemoteCertificateValidationCallback(System.Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+// 	{
+// 		return true;
+// 	}
+
+//     void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+// 	{
+// 		string msg = System.Text.Encoding.UTF8.GetString(e.Message);
+//         Debug.Log ("Received message from " + e.Topic + " : " + msg);
+// 	}
+
+//     private void Publish(string _topic, string msg)
+// 	{
+// 		client.Publish(
+// 			_topic, Encoding.UTF8.GetBytes(msg),
+// 			MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE, false);
+// 	}
+
+//     void OnDestroy() {
+//         client.Disconnect();
+//     }
+// }
+
