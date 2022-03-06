13795ba5fd085a9f5fdefb93cb38249025f13fef
xFlaye <eyala@yorku.ca>
2022-03-06 14:51:59 -0500

    updated functionality
    Added torus and sphere + controls

--- Graph/Assets/Scripts/FunctionLibrary.cs
+++ Graph/Assets/Scripts/FunctionLibrary.cs
@@ -10,9 +10,9 @@ public static class FunctionLibrary
     // public delegate float Function (float x, float z, float t);
     public delegate Vector3 Function (float u, float v, float t);
 
-    public enum FunctionName {Wave, MultiWave, Ripple, RippleChaos, TripleWave}
+    public enum FunctionName {Wave, MultiWave, Ripple, RippleChaos, TripleWave, Sphere, Torus}
 
-    static Function[] functions = {Wave, MultiWave, Ripple, RippleChaos, TripleWave};
+    static Function[] functions = {Wave, MultiWave, Ripple, RippleChaos, TripleWave, Sphere, Torus};
 
     // public static Function GetFunction (int index)
     // {
@@ -99,7 +99,7 @@ public static class FunctionLibrary
         Vector3 p;
         p.x = u;
         p.y = Sin(PI * (4f * d - t));
-        p.y /= (1f + Cos(10f * d)); // use a fixed amplitude (10)
+        p.y /= Sin(PI)+(1f + Cos(2f * d)); // use a fixed amplitude (10)
         p.z = v;
         return p;
     } 
@@ -117,6 +117,39 @@ public static class FunctionLibrary
         p.z = v;
         return p;
     }
+
+    public static Vector3 Sphere (float u, float v, float t) {
+		
+		// uniform
+        // float r = 0.5f + 0.5f * Sin(PI * t);
+
+        // non-uniform (flower with higher resolutions)
+        // float r = 0.9f + 0.1f * Sin(8f * PI * u);
+
+        // bands
+        float r = 0.9f + 0.1f * Sin(PI * (6f * u + 4f * v + t));
+
+		float s = r * Cos(0.5f * PI * v);
+		Vector3 p;
+		p.x = s * Sin(PI * u);
+		// onion shaped
+        p.y = v; 
+        // ball shaped
+        // p.y = r * Sin(PI * 0.5f * v);
+		p.z = s * Cos(PI * u);
+		return p;
+	}
+
+    public static Vector3 Torus (float u, float v, float t) {
+		float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+		float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+		float s = r1 + r2 * Cos(PI * v);
+		Vector3 p;
+		p.x = s * Sin(PI * u);
+		p.y = r2 * Sin(PI * v);
+		p.z = s * Cos(PI * u);
+		return p;
+	}
 
    
 
--- Graph/Assets/Scripts/Graph.cs
+++ Graph/Assets/Scripts/Graph.cs
@@ -19,7 +19,12 @@ public class Graph : MonoBehaviour
     Transform[] points;
     
     // public int power = 2;
-    public int divider = 2;
+    [SerializeField, Range(1,5)]
+    int divider = 2;
+    [SerializeField, Range(1,100)]
+    float stepAmount = 1f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,16 +37,19 @@ public class Graph : MonoBehaviour
         
         FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
-        float step = 2f / resolution;
+        float step = stepAmount / resolution;
+        int depthRest = resolution/divider;
+
+        float v = 0.5f * step - 1f;
     
         for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) 
         {
-            if (x == resolution) {
+            if (x == depthRest) {
                 x = 0;
                 z += 1;
+                v = (z + 0.5f) * step - 1f;
             }
             float u = (x + 0.5f) * step - 1f;
-            float v = (z + 0.5f) * step - 1f;
             points[i].localPosition = f(u, v, time);
 
                 // Transform point = points[i];
@@ -50,13 +58,13 @@ public class Graph : MonoBehaviour
             // position.y = f(position.x, position.z, time);
 
             // //! Y-axis options
-            // // change the shape of the parabola 
-            // // position.y = Mathf.Pow(position.x,power); 
+            // change the shape of the parabola 
+            // position.y = Mathf.Pow(position.x,power); 
 
             // //? use a sine wave (updated with a function)
-            // //position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            //position.y = Mathf.Sin(Mathf.PI * (position.x + time));
             
-            // // reapply the change to the point
+            // reapply the change to the point
             // point.localPosition = position;
         }
             
@@ -68,22 +76,16 @@ public class Graph : MonoBehaviour
 
     async void Awake()
     {
-        float step = 2f / resolution;
-        var position = Vector3.zero;
+        float step = stepAmount / resolution;
+        // var position = Vector3.zero;
         var scale = Vector3.one * step;
-        int depthRest = resolution/divider;
+        
 
         //! assign length of points in Transform array based on resolution
-        points = new Transform[resolution * depthRest];
+        points = new Transform[resolution * resolution];
         // instead of i < resolution
-        for (int i=0, x = 0, z = 0; i < points.Length; i++, x++)
+        for (int i = 0; i < points.Length; i++)
         {
-            // reset x back to zero
-            if (x == resolution) 
-            {
-                x = 0;
-                z += 1;
-            }
             // grab the prefab from the gameobject and instantiate it
             // Transform point = Instantiate(pointPrefab);
             // points[i] = point;
@@ -93,10 +95,10 @@ public class Graph : MonoBehaviour
             Transform point = points[i] = Instantiate(pointPrefab);
 
             // set X axis (align it so it's balanced along origin)
-            position.x = (x + 0.5f) * step - 1f;
-            position.z = (z + 0.5f) * step - 1f;
+            // position.x = (x + 0.5f) * step - 1f;
+            // position.z = (z + 0.5f) * step - 1f;
             
-            point.localPosition = position;
+            // point.localPosition = position;
             point.localScale = scale;
 
             // parent to gameObject
